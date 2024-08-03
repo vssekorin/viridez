2a88c82e52bd02d28bfb7504834cd7b225acdef0
Vseslav Sekorin <vssekorin@gmail.com>
2024-08-03 23:56:49 +0300

    Add liquibase

--- back/viridez/build.gradle.kts
+++ back/viridez/build.gradle.kts
@@ -2,8 +2,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 plugins {
-	id("org.springframework.boot") version "3.3.0"
-	id("io.spring.dependency-management") version "1.1.5"
+	id("org.springframework.boot") version "3.3.2"
+	id("io.spring.dependency-management") version "1.1.6"
 
 	val kotlinVersion = "2.0.0"
 	kotlin("plugin.jpa") version kotlinVersion
--- back/viridez/core/build.gradle.kts
+++ back/viridez/core/build.gradle.kts
@@ -1,5 +1,11 @@
 import org.springframework.boot.gradle.tasks.bundling.BootJar
 
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.liquibase:liquibase-core")
+    runtimeOnly("org.postgresql:postgresql")
+}
+
 tasks.withType<BootJar> {
     enabled = false
 }
